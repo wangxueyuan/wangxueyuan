41afe0b39490a8db12c3840c2bca21cd837ecb32
wangxueyuan <wangxueyuan05458@navinfo.com>
2020-06-11 11:25:47 +0800

    二分查找及其变种代码提交

--- java-data-structure/src/main/java/algorithm/BinarySearch.java
+++ java-data-structure/src/main/java/algorithm/BinarySearch.java
@@ -6,6 +6,7 @@ package algorithm;
  * @Version: 1.0
  **/
 public class BinarySearch {
+    //查找值等于目标数的位置
     public static int binarySearch(int[] arr, int low,int high, int target) {
         while (low <= high) {
             int middle = low + (high - low) / 2;
@@ -13,13 +14,106 @@ public class BinarySearch {
                 return middle;
             }
             if (arr[middle] < target) {
-                low = middle + 1;
+                low=middle+1;
+
             }
             if (arr[middle] > target) {
-                high = high - 1;
+                high = middle - 1;
             }
-            return middle;
+            return binarySearch(arr, low, high, target);
         }
         return -1;
+    }
+
+    //查找第一个等于目标数的位置
+    public static int binarySearchFistMatch(int[] arr, int low,int high, int target) {
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (arr[middle] == target) {
+                if(middle==0||arr[middle-1]!=target)
+                    return middle;
+                else
+                    high = middle - 1;
+            }
+
+            if (arr[middle] < target) {
+                low=middle+1;
+
+            }
+            if (arr[middle] > target) {
+                high = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    //查找最后一个等于目标数的位置
+    public static int binarySearchLastMatch(int[] arr, int low,int high, int target) {
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (arr[middle] == target) {
+                if(middle==arr.length-1||arr[middle+1]!=target)
+                    return middle;
+                else
+                    low = middle + 1;
+            }
+
+            if (arr[middle] < target) {
+                low=middle+1;
+
+            }
+            if (arr[middle] > target) {
+                high = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    //查找第一个大于等于目标数的位置
+    public static int binarySearchFistMatchOrOver(int[] arr, int low,int high, int target) {
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (arr[middle] >= target) {
+                if(middle==0||arr[middle-1]<target)
+                    return middle;
+                else
+                    high = middle - 1;
+            }
+
+            if (arr[middle] < target) {
+                low=middle+1;
+
+            }
+
+        }
+        return -1;
+    }
+
+    //查找最后一个小于等于目标数的位置
+    public static int binarySearchLastMatchOrBelow(int[] arr, int low,int high, int target) {
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (arr[middle] <= target) {
+                if(middle==arr.length-1||arr[middle+1]>target)
+                    return middle;
+                else
+                    low = middle + 1;
+            }
+
+
+            if (arr[middle] > target) {
+                high = middle - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] testArr = {34, 34, 35, 55, 68, 111, 1111, 1111, 2222, 3333, 4444, 4444, 4444};
+        System.out.println(binarySearch(testArr,0,testArr.length,111));
+        System.out.println(binarySearchFistMatch(testArr,0,testArr.length,34));
+        System.out.println(binarySearchLastMatch(testArr,0,testArr.length,34));
+        System.out.println(binarySearchFistMatchOrOver(testArr, 0, testArr.length, 78));
+        System.out.println(binarySearchLastMatchOrBelow(testArr,0,testArr.length,3335));
     }
 }
